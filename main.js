5fd6d4b9ca3087c321862d605f6cf06c8c305384
Ethan <141580717+tutucea@users.noreply.github.com>
2025-03-21 10:00:31 -1000

    Update main.js
    notification should work

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, Notification } = require('electron');
 const path = require('path');
 const fs = require('fs');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 325,
+    width: 465,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     }
   });
 
-  win.loadFile('index.html');
+  win.loadFile('index.html'); // You’ve fixed this
 }
 
 function loadSchedule(filename) {
@@ -22,57 +22,31 @@ function loadSchedule(filename) {
 app.whenReady().then(() => {
   createWindow();
 
-  ipcMain.handle('get-moon-schedule', async () => {
-    return loadSchedule('moon_schedule.json');
-  });
+  ipcMain.handle('get-moon-schedule', async () => loadSchedule('moon_schedule.json'));
+  ipcMain.handle('get-sun-schedule', async () => loadSchedule('sun_schedule.json'));
+  ipcMain.handle('get-nodes-schedule', async () => loadSchedule('nodes_schedule.json'));
+  ipcMain.handle('get-mercury-schedule', async () => loadSchedule('mercury_schedule.json'));
+  ipcMain.handle('get-venus-schedule', async () => loadSchedule('venus_schedule.json'));
+  ipcMain.handle('get-mars-schedule', async () => loadSchedule('mars_schedule.json'));
+  ipcMain.handle('get-jupiter-schedule', async () => loadSchedule('jupiter_schedule.json'));
+  ipcMain.handle('get-saturn-schedule', async () => loadSchedule('saturn_schedule.json'));
+  ipcMain.handle('get-uranus-schedule', async () => loadSchedule('uranus_schedule.json'));
+  ipcMain.handle('get-neptune-schedule', async () => loadSchedule('neptune_schedule.json'));
+  ipcMain.handle('get-pluto-schedule', async () => loadSchedule('pluto_schedule.json'));
 
-  ipcMain.handle('get-sun-schedule', async () => {
-    return loadSchedule('sun_schedule.json');
+  ipcMain.on('show-notification', (event, { title, body }) => {
+    const notification = new Notification({
+      title: title,
+      body: body,
+    });
+    notification.show();
   });
-
-  ipcMain.handle('get-nodes-schedule', async () => {
-    return loadSchedule('nodes_schedule.json');
-  });
-
-  ipcMain.handle('get-mercury-schedule', async () => {
-    return loadSchedule('mercury_schedule.json');
-  });
-
-  ipcMain.handle('get-venus-schedule', async () => {
-    return loadSchedule('venus_schedule.json');
-  });
-
-  ipcMain.handle('get-mars-schedule', async () => {
-    return loadSchedule('mars_schedule.json');
-  });
-
-  ipcMain.handle('get-jupiter-schedule', async () => {
-    return loadSchedule('jupiter_schedule.json');
-  });
-
-  ipcMain.handle('get-saturn-schedule', async () => {
-    return loadSchedule('saturn_schedule.json');
-  });
-
-  ipcMain.handle('get-uranus-schedule', async () => {
-    return loadSchedule('uranus_schedule.json');
-  });
-  ipcMain.handle('get-neptune-schedule', async () => {
-    return loadSchedule('neptune_schedule.json');
-  });
-  ipcMain.handle('get-pluto-schedule', async () => {
-    return loadSchedule('pluto_schedule.json');
-  })
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
